3c7dc17d5e0e001ceb3ae82ff01af2f5c4c5265b
Warren Wice <mulubiwarren@gmail.com>
2023-01-23 18:50:58 +0000

    Update index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,1 +1,8 @@
+#!/usr/bin/python3
+"""index.py to connect to API"""
+from api.v1.views import app_views
 
+@app_views.route('/status', strict_slashes=False)
+def hbnbStatus():
+    """hbnbStatus"""
+    return jsonify({"status": "OK"})
