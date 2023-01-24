bb575efb951b9e233a42d28ec7cd23cb0731a56b
Warren Wice <mulubiwarren@gmail.com>
2023-01-24 19:29:07 +0000

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,27 +3,43 @@
 from models import storage
 from api.v1.views import app_views
 from os import environ
-from flask import Flask, render_template, jsonify, make_response
+from flask import Flask, render_template, make_response, jsonify
 from flask_cors import CORS
+from flasgger import Swagger
+from flasgger.utils import swag_from
 
 app = Flask(__name__)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views)
 cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
-def close_storage(error):
-    """teardown_appcontext or close storage"""
+def close_db(error):
+    """ Close Storage """
     storage.close()
 
 
 @app.errorhandler(404)
-def page_not_found(error):
-    """Raises 404 error"""
+def not_found(error):
+    """ 404 Error
+    ---
+    responses:
+      404:
+        description: a resource was not found
+    """
     return make_response(jsonify({'error': "Not found"}), 404)
 
+app.config['SWAGGER'] = {
+    'title': 'AirBnB clone Restful API done by Warren and Joe',
+    'uiversion': 3
+}
+
+Swagger(app)
+
+
 if __name__ == "__main__":
-    """Displays the Maine Function"""
+    """ Main Function """
     host = environ.get('HBNB_API_HOST')
     port = environ.get('HBNB_API_PORT')
     if not host:
