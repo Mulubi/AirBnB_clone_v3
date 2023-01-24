91d7f2b8e2157e1349c1b71e03ce58ea9d8398b4
Mulubi <mulubiwarren@gmail.com>
2023-01-24 17:38:15 +0000

    Debugged task 3

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -12,3 +12,6 @@ from api.v1.views.cities import *
 from api.v1.views.places import *
 from api.v1.views.places_amenities import *
 from api.v1.views.places_reviews import *
+
+if __name__ == "__main__":
+    pass
