1b963c1414a4a9856ba85cdba1951c43aa2ff25d
Joseph Mumo <51504499+JosephMumo@users.noreply.github.com>
2023-01-24 23:17:39 +0300

    init

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Blueprint for the API Application"""
+"""Blueprint for our API Application"""
 from flask import Blueprint
 
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
