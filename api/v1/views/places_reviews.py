746ff369ee968adfc15cd2f829c550b4d3472c95
Joseph Mumo <51504499+JosephMumo@users.noreply.github.com>
2023-01-24 23:22:31 +0300

    init

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" These objects handle all default RestFul API actions for Reviews """
+""" These objects handle all the default RestFul API actions for Reviews """
 from models.review import Review
 from models.place import Place
 from models.user import User
