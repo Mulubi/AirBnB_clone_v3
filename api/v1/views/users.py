3d18b3866df931d6ed7387ddbb7f171874515cce
Joseph Mumo <51504499+JosephMumo@users.noreply.github.com>
2023-01-24 23:23:41 +0300

    init

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" These objects handle all default RestFul API actions for Users """
+""" These objects handle all the default RestFul API actions for Users """
 from models.user import User
 from models import storage
 from api.v1.views import app_views
